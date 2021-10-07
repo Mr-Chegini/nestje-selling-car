2a39b9d2c0f63fcaa92c72025ed8d524ab924759
Amir Chegini <amirchegini007@gmail.com>
2021-10-07 18:10:07 +0330

    Serialize Data with UseIterceptors Decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,8 @@ import {
   Delete,
   Query,
   Param,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -21,21 +23,25 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
   findUser(@Param('id') id: string) {
     return this.usersService.findOne(parseInt(id));
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get()
   findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Delete('/:id')
   removeUser(@Param('id') id: string) {
     return this.usersService.remove(parseInt(id));
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Patch('/:id')
   updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
     return this.usersService.update(parseInt(id), body);
